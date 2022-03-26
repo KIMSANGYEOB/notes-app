e7bea1412e90d0605a33bdb0a86f43ab1ef649e6
KIMSANGYEOB <sykim10070@gmail.com>
2022-03-26 23:46:50 +0900

    서버리스 파트-2 커밋

--- src/create.ts
+++ src/create.ts
@@ -1,9 +1,10 @@
-import * as uuid from "uuid"
-import AWS from "aws-sdk";
+import * as uuid from 'uuid';
+import AWS from 'aws-sdk';
+import { APIGatewayProxyResult } from 'aws-lambda';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export async function main(event: any) {
+export async function main(event: any): Promise<APIGatewayProxyResult> {
   // JSON으로 데이터를 넘겨 'event.body' 에서 파싱이 필요하다
   const data = JSON.parse(event.body);
 
@@ -11,7 +12,7 @@ export async function main(event: any) {
     TableName: process.env.TABLE_NAME!,
     Item: {
       // 실제 DynamoDB에 저장되는 정보들
-      userId: "123", // 사용자 ID
+      userId: '123', // 사용자 ID
       noteId: uuid.v1(), // 메모를 생성할 때 생성되는 고유ID (uuid 라이브러리 사용)
       content: data.content, // event.body 쪽에서 받은 정보
       attachment: data.attachment, // event.body 쪽에서 받은 정보
