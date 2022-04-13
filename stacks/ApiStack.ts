58a2dedfaee0ecc4058de9644ffb23a20105b1a2
KIMSANGYEOB <sykim10070@gmail.com>
2022-04-13 23:40:10 +0900

    auth 관련 api 작성 및 적용

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,5 +1,6 @@
 // stacks/ApiStack.ts
 import * as sst from '@serverless-stack/resources';
+import { ApiAuthorizationType } from '@serverless-stack/resources';
 
 export default class ApiStack extends sst.Stack {
   // 다른 스택에서 접근할 수 있도록 선언
@@ -12,6 +13,8 @@ export default class ApiStack extends sst.Stack {
 
     // API 생성 (두번째 인자에 본인 이니셜을 포함해 ID를 넣어주자)
     this.api = new sst.Api(this, 'api-sykim', {
+      // AWS_IAM 인증하도록 설정
+      defaultAuthorizationType: ApiAuthorizationType.AWS_IAM,
       defaultFunctionProps: {
         environment: {
           TABLE_NAME: table.tableName,
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,16 +1,24 @@
 // stacks/index.ts
-import * as sst from "@serverless-stack/resources";
-import StorageStack from "./StorageStack";
-import ApiStack from "./ApiStack";
+import * as sst from '@serverless-stack/resources';
+import StorageStack from './StorageStack';
+import ApiStack from './ApiStack';
+import AuthStack from './AuthStack';
 
 export default function main(app: sst.App): void {
   // 두번째 인자에 본인 이름이나 이니셜로 생성해주자
   // ex) storage-sykim
-  const storageStack = new StorageStack(app, "storage-sykim");
+  const storageStack = new StorageStack(app, 'storage-sykim');
 
   // 두번째 인자에 본인 이름이나 이니셜로 생성해주자
   // ex) api-sykim
-  new ApiStack(app, "api-sykim", {
+  const apiStack = new ApiStack(app, 'api-sykim', {
     table: storageStack.table,
+  });
+
+  // 두번째 인자에 본인 이름이나 이니셜로 생성해주자
+  // ex) auth-sykim
+  new AuthStack(app, 'auth', {
+    api: apiStack.api,
+    bucket: storageStack.bucket,
   });
 }
