6c7bd673b3447c68c43d65c7874e3756a5101c37
KIMSANGYEOB <sykim10070@gmail.com>
2022-04-18 17:57:52 +0900

    Frontend Stack 구성

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,8 +1,8 @@
-// stacks/index.ts
 import * as sst from '@serverless-stack/resources';
 import StorageStack from './StorageStack';
 import ApiStack from './ApiStack';
 import AuthStack from './AuthStack';
+import FrontendStack from "./FrontendStack";
 
 export default function main(app: sst.App): void {
   // 두번째 인자에 본인 이름이나 이니셜로 생성해주자
@@ -17,8 +17,14 @@ export default function main(app: sst.App): void {
 
   // 두번째 인자에 본인 이름이나 이니셜로 생성해주자
   // ex) auth-sykim
-  new AuthStack(app, 'auth', {
+  const authStack = new AuthStack(app, 'auth', {
     api: apiStack.api,
+    bucket: storageStack.bucket,
+  });
+
+  new FrontendStack(app, "frontend", {
+    api: apiStack.api,
+    auth: authStack.auth,
     bucket: storageStack.bucket,
   });
 }
