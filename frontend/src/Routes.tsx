84962812e0ff25f97fbf5bcd57f3aa17d50ecc05
KIMSANGYEOB <sykim10070@gmail.com>
2022-05-13 00:01:41 +0900

    서버리스 메모 앱 프로젝트 완성

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,33 +1,35 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import Login from "./containers/Login";
-import Signup from "./containers/Signup";
-import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
-import NotFound from "./containers/NotFound";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+import NotFound from './containers/NotFound';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 export default function Routes() {
   return (
     <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route exact path="/login">
-            <Login />
-        </Route>
-        <Route exact path="/signup">
-            <Signup />
-        </Route>
-        <Route exact path="/notes/new">
-            <NewNote />
-        </Route>
-        <Route exact path="/notes/:id">
-            <Notes />
-        </Route>
-        <Route>
-            <NotFound />
-        </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
